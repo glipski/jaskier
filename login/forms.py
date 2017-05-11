e680877924b02205f8466e1a6e747c8699a9e723
jaskier234 <jaskier234@gmail.com>
2017-05-11 16:01:52 +0200

    szaro-niebeski motyw

--- login/forms.py
+++ login/forms.py
@@ -3,10 +3,10 @@ from django import forms
 class LoginForm(forms.Form):
 	username = forms.CharField(
 		max_length=100,
-		widget=forms.TextInput(attrs={'placeholder':'login'}),
+		widget=forms.TextInput(attrs={'placeholder':' login'}),
 	)
 	password = forms.CharField(
-		widget=forms.PasswordInput(attrs={'placeholder':'hasło'}),
+		widget=forms.PasswordInput(attrs={'placeholder':' hasło'}),
 	)
 
 
