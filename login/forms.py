b6c288d9034b22808e70c2b899729db079ad7bfc
jaskier234 <jaskier234@gmail.com>
2017-05-11 07:42:55 +0200

    wygląd strony logowania

--- login/forms.py
+++ login/forms.py
@@ -1,7 +1,12 @@
 from django import forms
 
 class LoginForm(forms.Form):
-	username = forms.CharField(max_length=100)
-	password = forms.CharField(widget=forms.PasswordInput())
+	username = forms.CharField(
+		max_length=100,
+		widget=forms.TextInput(attrs={'placeholder':'login'}),
+	)
+	password = forms.CharField(
+		widget=forms.PasswordInput(attrs={'placeholder':'hasło'}),
+	)
 
 
